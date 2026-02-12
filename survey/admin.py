f17770199eccd05016c6909c04a7602083069b6a
mocriezaldy <dosdiosa22@gmail.com>
2026-02-12 15:45:44 +0700

    menambahkan semua tabel di admin

--- survey/admin.py
+++ survey/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Survey
 
-admin.site.register(Survey)
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Survey._meta.fields]
